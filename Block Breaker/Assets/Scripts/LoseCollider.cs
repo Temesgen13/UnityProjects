04c0f698c12192249710c89f4aa9088c37ce656a
Temesgen13 <temesgen13@gmail.com>
2018-03-28 21:18:43 -0700

    Blocker breaker level 0 updates
    added background, ball, and colliders. only print ot console now

---
+++ Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter2D (Collider2D trigger){
+		print ("Trigger");
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision){
+		print ("Collision");
+	}	
+}
