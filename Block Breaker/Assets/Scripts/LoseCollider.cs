b5a33d85254044e8c2ce27863b50117553a086b8
Temesgen13 <temesgen13@gmail.com>
2018-03-30 22:38:41 -0700

    Added Colliders
    add colliders and trigger to ball and empy object. ball destroyed on exit now.

--- Block Breaker/Assets/Scripts/LoseCollider.cs
+++ Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -2,10 +2,11 @@
 using System.Collections;
 
 public class LoseCollider : MonoBehaviour {
-
+	public LevelManager levelManager;
 	// Use this for initialization
 	void OnTriggerEnter2D (Collider2D trigger){
 		print ("Trigger");
+		levelManager.LoadLevel("Win");
 	}
 	
 	void OnCollisionEnter2D (Collision2D collision){
